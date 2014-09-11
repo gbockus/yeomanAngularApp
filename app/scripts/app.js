668aea2850002fb176a15b35aa8e558949fcdb96
Gordon Bockus <gbockus@spanning.com>
2014-09-10 21:38:32 -0500

    Initial commit.  Adding all files created by the yeoman angular-genertor

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularYeomanAppApp
+ * @description
+ * # angularYeomanAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularYeomanAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularYeomanAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularYeomanAppApp
+ */
+angular.module('angularYeomanAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
